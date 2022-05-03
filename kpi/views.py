61f30b707882ab914588e40584fda278455a101a
YUNSANGMIN <74187642+redtea89@users.noreply.github.com>
2022-05-03 22:05:26 +0900

    FIX: Restaurant KPI 필수&옵션 조건 반영

--- kpi/views.py
+++ kpi/views.py
@@ -30,7 +30,8 @@ class RestaurantKpiView(APIView):
         # - start ~ end filter요소
         start_time = request.GET.get('start-time', None)
         if start_time is None :
-            return Response({"message":"start-time 쿼리를 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message":"start-time 쿼리를 입력하세요. \
+                            주의) 만약 2022-04-08일까지 확인하고 싶다면 2022-04-08 23:59:59를 입력하셔야합니다."}, status=status.HTTP_400_BAD_REQUEST)
         end_time = request.GET.get('end-time', None)
         if end_time is None :
             return Response({"message":"end-time 쿼리를 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
@@ -40,12 +41,12 @@ class RestaurantKpiView(APIView):
         try:
             start_time = datetime.strptime(start_time, time_format)
         except:
-            return Response({f"message":" start-time 입력형식은 YYYY-MM-DD hh:mm:ss 입니다."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message":" start-time 입력형식은 YYYY-MM-DD hh:mm:ss 입니다."}, status=status.HTTP_400_BAD_REQUEST)
         try:
             end_time = datetime.strptime(end_time, time_format)
         except:
-            return Response({f"message":" end-time 입력형식은 YYYY-MM-DD hh:mm:ss 입니다. \
-                                    주의) 2022-04-08일까지 확인하고 싶다면 2022-04-08 23:59:59를 입력하셔야합니다."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message":" end-time 입력형식은 YYYY-MM-DD hh:mm:ss 입니다. \
+                            주의) 만약 2022-04-08일까지 확인하고 싶다면 2022-04-08 23:59:59를 입력하셔야합니다."}, status=status.HTTP_400_BAD_REQUEST)
         # - filter 적용
         pos = pos.filter(timestamp__gte=start_time, timestamp__lte=end_time)
 
@@ -60,31 +61,30 @@ class RestaurantKpiView(APIView):
 
         # - start ~ end filter 요소  + integer인지 확인하는 요소
         start_price = request.GET.get('start-price', None)
-        if start_price is None :
-            return Response({"message":"start-price를 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            start_price = int(start_price)
-        except:
-            return Response({"message":"start-price는 자연수를 입력하세요. "}, status=status.HTTP_400_BAD_REQUEST)
-        if start_price < 0:
-            return Response({"message":"숫자 0 이상 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
+        if start_price is not None :
+            try:
+                start_price = int(start_price)
+            except:
+                return Response({"message":"start-price는 자연수를 입력하세요. "}, status=status.HTTP_400_BAD_REQUEST)
+            if start_price < 0:
+                return Response({"message":"숫자 0 이상 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
 
-        end_price = request.GET.get('end-price', None)
-        if end_price is None :
-            return Response({"message":"end-price를 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            end_price = int(end_price)
-        except:
-            return Response({"message":"end-price는 자연수를 입력하세요. "}, status=status.HTTP_400_BAD_REQUEST)
-        if end_price < 0:
-            return Response({"message":"숫자 0 이상 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
+            end_price = request.GET.get('end-price', None)
+            if end_price is None :
+                return Response({"message":"end-price를 입력하세요. start-price를 입력했다면 함께 넣어야 하는 요소입니다."}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                end_price = int(end_price)
+            except:
+                return Response({"message":"end-price는 자연수를 입력하세요. "}, status=status.HTTP_400_BAD_REQUEST)
+            if end_price < 0:
+                return Response({"message":"숫자 0 이상 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # - integer요소는 start와 end의 대소비교
-        if start_price > end_price:
-            return Response({"message":"start-price는 end-price보다 작아야합니다."}, status=status.HTTP_400_BAD_REQUEST)
+            # - integer요소는 start와 end의 대소비교
+            if start_price > end_price:
+                return Response({"message":"start-price는 end-price보다 작아야합니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # - filter 적용
-        pos = pos.filter(price__gte=start_price, price__lte=end_price)
+            # - filter 적용
+            pos = pos.filter(price__gte=start_price, price__lte=end_price)
 
         '''
         # [옵션] filter 3) number_of_party
@@ -94,31 +94,30 @@ class RestaurantKpiView(APIView):
         - filter 적용
         '''
         start_number_of_people = request.GET.get('start-number-of-people', None)
-        if start_number_of_people is None :
-            return Response({"message":"start-number-of-people를 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            start_number_of_people = int(start_number_of_people)
-        except:
-            return Response({"message":"start-number-of-people은 자연수를 입력하세요. "}, status=status.HTTP_400_BAD_REQUEST)
-        if start_number_of_people < 0:
-            return Response({"message":"숫자 0 이상 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
+        if start_number_of_people is not None :
+            try:
+                start_number_of_people = int(start_number_of_people)
+            except:
+                return Response({"message":"start-number-of-people은 자연수를 입력하세요. "}, status=status.HTTP_400_BAD_REQUEST)
+            if start_number_of_people < 0:
+                return Response({"message":"숫자 0 이상 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
 
-        end_number_of_people = request.GET.get('end-number-of-people', None)
-        if end_number_of_people is None :
-            return Response({"message":"end-number-of-people를 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            end_number_of_people = int(end_number_of_people)
-        except:
-            return Response({"message":"end-number-of-people은 자연수를 입력하세요. "}, status=status.HTTP_400_BAD_REQUEST)
-        if end_number_of_people < 0:
-            return Response({"message":"숫자 0 이상 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
+            end_number_of_people = request.GET.get('end-number-of-people', None)
+            if end_number_of_people is None :
+                return Response({"message":"end-number-of-people를 입력하세요. start-number-of-people를(을) 입력했다면 함께 넣어야 하는 요소입니다."}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                end_number_of_people = int(end_number_of_people)
+            except:
+                return Response({"message":"end-number-of-people은 자연수를 입력하세요. "}, status=status.HTTP_400_BAD_REQUEST)
+            if end_number_of_people < 0:
+                return Response({"message":"숫자 0 이상 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # - integer요소는 start와 end의 대소비교
-        if start_number_of_people > end_number_of_people:
-            return Response({"message":"start-number_of_people는 end-number_of_people보다 작아야합니다."}, status=status.HTTP_400_BAD_REQUEST)
+            # - integer요소는 start와 end의 대소비교
+            if start_number_of_people > end_number_of_people:
+                return Response({"message":"start-number_of_people는 end-number_of_people보다 작아야합니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # - filter 적용
-        pos = pos.filter(number_of_party__gte=start_number_of_people, number_of_party__lte=end_number_of_people)
+            # - filter 적용
+            pos = pos.filter(number_of_party__gte=start_number_of_people, number_of_party__lte=end_number_of_people)
 
         '''
         # [옵션] filter 4) restaurant_group
@@ -127,17 +126,18 @@ class RestaurantKpiView(APIView):
         - 쿼리 받아서 리스트와 비교하기
         - 필터 적용하기
         '''
-        # - 쿼리 리스트 가져오기
+        # - 쿼리 받기
+        restaurant_group = request.GET.get('restaurant-group', None)
+
+        # - 비교할 리스트 가져오기
         group_name_list = ['비비고','빕스버거']
         
-        # - 쿼리 받아서 리스트와 비교하기
-        restaurant_group = request.GET.get('restaurant-group', None)
-        if restaurant_group is None :
-            return Response({"message":"restaurant-group 쿼리를 입력하세요."}, status=status.HTTP_400_BAD_REQUEST)
-        if not restaurant_group in group_name_list:
-            return Response({"message":"restaurant-group의 입력 인자는 '비비고','빕스버거' 중의 하나입니다."}, status=status.HTTP_400_BAD_REQUEST)
-        search_group = Restaurant.objects.filter(restaurant_name=restaurant_group).values('id')
-        pos = pos.filter(restaurant_id__in=search_group)
+        #  옵션조건이기에 None인지 확인한 후 리스트와 비교하기
+        if restaurant_group is not None :
+            if not restaurant_group in group_name_list:
+                return Response({"message":"restaurant-group의 입력 인자는 '비비고','빕스버거' 중의 하나입니다."}, status=status.HTTP_400_BAD_REQUEST)
+            search_group = Restaurant.objects.filter(restaurant_name=restaurant_group).values('id')
+            pos = pos.filter(restaurant_id__in=search_group)
 
         '''
         # [필수] Time window
