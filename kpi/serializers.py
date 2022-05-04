bb94da5d2e263565740e15fb411423d642a73459
YUNSANGMIN <74187642+redtea89@users.noreply.github.com>
2022-05-04 14:45:51 +0900

    FIX: 에러코드 수정

--- kpi/serializers.py
+++ kpi/serializers.py
@@ -23,7 +23,7 @@ class RestaurantKpiSerializer(serializers.ModelSerializer):
         두 번째 변수 : 쿼리명 string
         '''
         if var is None :
-            raise ValidationError(f"message':{msg} 쿼리를 입력하세요.")
+            raise ValidationError(f"{msg} 쿼리를 입력하세요.")
 
     def change_format_to_datetime(self, input_time, msg, time_format, input_exapmle):
         """
@@ -36,7 +36,7 @@ class RestaurantKpiSerializer(serializers.ModelSerializer):
         try:
             input_time = datetime.strptime(input_time, time_format)
         except:
-            raise ValidationError(f"message': {msg} 입력형식은 {input_exapmle} 입니다.")
+            raise ValidationError(f"{msg} 입력형식은 {input_exapmle} 입니다.")
 
             ##  is_integer
     def is_zero_or_more_numbers(self, var, msg):
@@ -48,9 +48,9 @@ class RestaurantKpiSerializer(serializers.ModelSerializer):
         try:
             var = int(var)
         except:
-            raise ValidationError(f"message':{msg}는 0 이상의 숫자 입력하세요.")
+            raise ValidationError(f"{msg}는 0 이상의 숫자 입력하세요.")
         if var < 0:
-            raise ValidationError(f"message':{msg}는 0 이상의 숫자 입력하세요.")
+            raise ValidationError(f"{msg}는 0 이상의 숫자 입력하세요.")
 
     def is_equal_or_larger_size(self, small_val, large_val):
         """
@@ -59,7 +59,7 @@ class RestaurantKpiSerializer(serializers.ModelSerializer):
         두 번째 변수 : 큰 값
         """
         if large_val < small_val:
-            raise ValidationError(f"message':{large_val} >= {small_val} 조건을 만족해야 합니다.")
+            raise ValidationError(f"{large_val} >= {small_val} 조건을 만족해야 합니다.")
 
 class PaymentKpiSerializer(serializers.ModelSerializer):
     """
