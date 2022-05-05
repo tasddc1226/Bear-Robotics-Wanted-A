7e65118a9ef82a9ea1e3c7abf482c8c63dac51eb
tasddc1226 <tasddc@naver.com>
2022-05-05 20:36:59 +0900

    FEAT: 점포 별 kpi 수치 api 리펙터링 및 리뷰반영 작업

--- kpi/views.py
+++ kpi/views.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from restaurant.models import Restaurant, RestaurantGroup, RestaurantMenu
 from .models import PosResultData
 from .serializers import (
@@ -14,10 +15,16 @@ from typing import Dict
 from .validate import *
 class RestaurantKpiView(APIView):
     """
-    A REST API to show KPI sales (price) per restaurant
-    Only GET method exsists.
+        Writer : 윤상민
+        Reviewer : 양수영
+        Refactor : 윤상민, 양수영
     """
     def get(self, request, format=None):
+        """
+            A REST API to show KPI sales (price) per restaurant
+            Only GET method exsists.
+            base url: http://127.0.0.1:8000/api/v1/kpi/restaurant
+        """
 
         """ [NECESSARY] query params """
         start_time  = request.GET.get('start-time', None)
@@ -35,50 +42,75 @@ class RestaurantKpiView(APIView):
         """ [OPTIONAL 3] query params """
         restaurant_group = request.GET.get('restaurant-group', None)
 
-        """ [OPTIONAL 4] query params """
+        """ [OPTIONAL 4] query params """ # 결제 수단은 아래 쪽으로 이동
         payment = request.GET.get('payment', None)
-
-        """ [OPTIONAL 5] query params """
-        number_of_party = request.GET.get('number-of-party', None)
         
-        if not all([start_time, end_time]):
-            return Response(
-                {"message":"start date 또는 end date를 입력하세요."},
-                status = status.HTTP_400_BAD_REQUEST
-            )
+        # Validate start_time, end_time params
+        check_none_necessary_string(start_time, "start_time")
+        check_none_necessary_string(end_time, "end_time")
+        
+        # Change date format
+        start_time = change_format_to_datetime(start_time, 'start-time','%Y-%m-%d', 'YYYY-MM-DD')
+        end_time = change_format_to_datetime(end_time, 'end-time' ,'%Y-%m-%d', 'YYYY-MM-DD')
+        
+        is_equal_or_larger_size(start_time, end_time)
 
         time_window_archive = {
-            'hour': TruncHour('timestamp'),
-            'day': TruncDay('timestamp'),
-            'week': TruncWeek('timestamp'),
+            'hour' : TruncHour('timestamp'),
+            'day'  : TruncDay('timestamp'),
+            'week' : TruncWeek('timestamp'),
             'month': TruncMonth('timestamp'),
-            'year': TruncYear('timestamp')
+            'year' : TruncYear('timestamp')
         }
+        
+        # Validate time_window params
+        check_none_necessary_string(time_window, "time-window")
         if not time_window in time_window_archive:
             return Response(
-                {"message":"time-window의 입력 인자는 'hour','day','week','month','year'중의 하나입니다."},
+                {"message":"time-window의 입력 인자는 'HOUR','DAY','WEEK','MONTH','YEAR'중의 하나입니다."},
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        # Change date format
-        start_time = change_format_to_datetime(self, start_time, 'start-time','%Y-%m-%d', 'YYYY-MM-DD')
-        end_time = change_format_to_datetime(self, end_time, 'end-time' ,'%Y-%m-%d', 'YYYY-MM-DD')
-        print(start_time, end_time)
+        if (start_price is not None) or (end_price is not None):
+            check_none_necessary_string(start_price, 'start-price')
+            check_none_necessary_string(end_price, 'end-price')
+            is_zero_or_more_numbers(start_price, 'start-price')
+            is_zero_or_more_numbers(end_price, 'end-price')
+            is_equal_or_larger_size(int(start_price), int(end_price))
+
+        if (start_number_of_people is not None) or (end_number_of_people is not None):
+            check_none_necessary_string(start_number_of_people, 'start-number-of-people')
+            check_none_necessary_string(end_number_of_people, 'end-number-of-people')
+            is_zero_or_more_numbers(start_number_of_people, 'start-number-of-people')
+            is_zero_or_more_numbers(end_number_of_people, 'end-number-of-people')
+            is_equal_or_larger_size(int(start_number_of_people), int(end_number_of_people))
+
+        if (restaurant_group is not None):
+            check_none_necessary_string(restaurant_group, 'restaurant_group')
+            is_zero_or_more_numbers(restaurant_group, 'restaurant_group')
+            restaurant = Restaurant.objects.filter(group=restaurant_group).first()
+            if not restaurant:
+                return Response(
+                    {"message":"찾고자 하는 점포의 그룹이 없습니다."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
         
-        queryset = Q()
-        queryset &= Q(timestamp__range=(start_time,end_time))
-        queryset = queryset & Q(price__gte=start_price) if start_price else queryset
-        queryset = queryset & Q(price__gte=end_price) if end_price else queryset
-        queryset = queryset & Q(number_of_party__gte=start_number_of_people) if start_number_of_people else queryset
-        queryset = queryset & Q(number_of_party__lte=end_number_of_people) if end_number_of_people else queryset
-        # TODO: group id에 대한 Q filter 적용하기
+        q = Q()
+        q &= Q(timestamp__range=(start_time, end_time + timedelta(days=1)))
 
-        result_data_set: Dict[str, object] = {}
-        result_data_set = PosResultData.objects.filter(queryset)\
-            .annotate(date=time_window_archive[time_window]).values('date')\
-                .annotate(restaurant_id=F('restaurant'), total_price=Sum('price'))\
-                    .values('date', 'restaurant_id', 'total_price')
+        if start_price and end_price:
+            q &= Q(price__range=(start_price, end_price))
+        
+        if start_number_of_people and end_number_of_people:
+            q &= Q(number_of_party__range=(start_number_of_people, end_number_of_people))
+        
+        if restaurant_group:
+            q &= Q(restaurant__group=restaurant_group)
 
+        result_data_set = PosResultData.objects.filter(q)\
+                                                .annotate(date=time_window_archive[time_window]).values('date')\
+                                                .annotate(restaurant_id=F('restaurant'), total_price=Sum('price'))\
+                                                .values('date', 'restaurant_id', 'total_price')
         return Response(result_data_set, status=status.HTTP_200_OK)
 
 class PaymentKpiView(APIView):
