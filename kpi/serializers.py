103f04bf3ba869f2cb866b9d4bd42f642f7c344b
tasddc1226 <tasddc@naver.com>
2022-05-06 11:49:45 +0900

    FEAT: 결재 방법 별 kpi 수치 api 추가

--- kpi/serializers.py
+++ kpi/serializers.py
@@ -26,12 +26,19 @@ class RestaurantKpiSerializer(serializers.Serializer):
         )
         read_only_fields = []
 
-class PaymentKpiSerializer(serializers.ModelSerializer):
+class PaymentKpiSerializer(serializers.Serializer):
     """
     PaymentKpiView Serializer
     """
-    term = serializers.DateTimeField()
-    count = serializers.IntegerField()
+    start_time  = serializers.CharField(help_text='검색 시작 날짜', required=True)
+    end_time    = serializers.CharField(help_text='검색 종료 날짜', required=True)
+    time_window = serializers.CharField(help_text='검색 범위', required=True)
+    start_price = serializers.CharField(help_text='시작 가격', required=False)
+    end_price   = serializers.CharField(help_text='종료 가격', required=False)
+    start_number_of_people = serializers.CharField(help_text='시작 구성원 수', required=False)
+    end_number_of_people   = serializers.CharField(help_text='종료 구성원 수', required=False)
+    restaurant_group = serializers.CharField(help_text='레스토랑 그룹 번호', required=False)
+    payment = serializers.CharField(help_text='결재 방법', required=True)
     
     class Meta:
         model = PosResultData
