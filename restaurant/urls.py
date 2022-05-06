6d8894aa382f0d365d53f739bb8394ee1d9c0c9c
YUNSANGMIN <74187642+redtea89@users.noreply.github.com>
2022-05-06 11:00:51 +0900

    CHORE: url name=변경

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,8 +5,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path('pos', PosDataListView.as_view(), name='pos_list'),
-    path('pos/<int:pk>', PosDataDetailView.as_view(), name='pos_detail'),
-    path('restaurants', RestaurantListView.as_view(), name='restaurant_list'),
-    path('restaurants/<int:pk>', RestaurantDetailView.as_view(), name='restaurnt_detail'),
+    path('pos', PosDataListView.as_view(), name='pos-list'),
+    path('pos/<int:pk>', PosDataDetailView.as_view(), name='pos-detail'),
+    path('restaurants', RestaurantListView.as_view(), name='restaurant-list'),
+    path('restaurants/<int:pk>', RestaurantDetailView.as_view(), name='restaurnt-detail'),
 ]
