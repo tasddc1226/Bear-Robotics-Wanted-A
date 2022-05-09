446f484353378bd064326358a4e6f87fa3ce7d78
YUNSANGMIN <74187642+redtea89@users.noreply.github.com>
2022-05-09 12:38:51 +0900

    FIX: kpi time_window 표시 문제 임시 해결

--- kpi/serializers.py
+++ kpi/serializers.py
@@ -14,17 +14,6 @@ class RestaurantKpiSerializer(serializers.Serializer):
     end_number_of_people   = serializers.CharField(help_text='종료 구성원 수', required=False)
     restaurant_group = serializers.CharField(help_text='레스토랑 그룹 번호', required=False)
 
-    class Meta:
-        model = PosResultData
-        fields = (
-            "id",
-            "timestamp",
-            "restaurant",
-            "price",
-            "number_of_party",
-            "payment",
-        )
-        read_only_fields = []
 
 class PaymentKpiSerializer(serializers.Serializer):
     """
@@ -38,22 +27,20 @@ class PaymentKpiSerializer(serializers.Serializer):
     start_number_of_people = serializers.CharField(help_text='시작 구성원 수', required=False)
     end_number_of_people   = serializers.CharField(help_text='종료 구성원 수', required=False)
     restaurant_group = serializers.CharField(help_text='레스토랑 그룹 번호', required=False)
-    payment = serializers.CharField(help_text='결재 방법', required=True)
-    
-    class Meta:
-        model = PosResultData
-        fields = ['term','restaurant_id','payment','count']
-        read_only_fields = []
+    payment = serializers.CharField(help_text='결제 방법', required=False)
 
-class PartyNumberKpiSerializer(serializers.ModelSerializer):
+
+class PartyNumberKpiSerializer(serializers.Serializer):
     """
     PartyNumberKpiView Serializer
     """
-    term = serializers.DateTimeField()
-    count = serializers.IntegerField()
+    start_time  = serializers.CharField(help_text='검색 시작 날짜', required=True)
+    end_time    = serializers.CharField(help_text='검색 종료 날짜', required=True)
+    time_window = serializers.CharField(help_text='검색 범위', required=True)
+    start_price = serializers.CharField(help_text='시작 가격', required=False)
+    end_price   = serializers.CharField(help_text='종료 가격', required=False)
+    start_number_of_people = serializers.CharField(help_text='시작 구성원 수', required=False)
+    end_number_of_people   = serializers.CharField(help_text='종료 구성원 수', required=False)
+    restaurant_group = serializers.CharField(help_text='레스토랑 그룹 번호', required=False)
+    payment = serializers.CharField(help_text='결제 방법', required=False)
     
-    class Meta:
-        model = PosResultData
-        fields = ['term','restaurant_id','number_of_party','count']
-        read_only_fields = []
-
