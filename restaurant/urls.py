951856a9d8d36a0d3a64f0ebe6077d0744e2975c
YUNSANGMIN <74187642+redtea89@users.noreply.github.com>
2022-05-09 12:18:49 +0900

    CHORE: feedback 반영

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,6 +7,6 @@ from .views import (
 urlpatterns = [
     path('pos', PosDataListView.as_view(), name='pos-list'),
     path('pos/<int:pk>', PosDataDetailView.as_view(), name='pos-detail'),
-    path('restaurants', RestaurantListView.as_view(), name='restaurant-list'),
-    path('restaurants/<int:pk>', RestaurantDetailView.as_view(), name='restaurnt-detail'),
+    path('', RestaurantListView.as_view(), name='restaurant-list'),
+    path('<int:pk>', RestaurantDetailView.as_view(), name='restaurnt-detail'),
 ]
